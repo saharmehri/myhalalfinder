9c6eb5a93996948be38e1712bb87ad75455f3208
Sahar Mehri <smehri@alumni.scu.edu>
2022-10-25 18:04:04 -0700

    beginning server.py with homepage.html

--- seed_database.py
+++ seed_database.py
@@ -1,8 +1,15 @@
 """Script to seed database"""
 
 import os
+import json
 
 import crud
 import model
 import server
+
+os.system('dropdb project')
+os.system('createdb project')
+
+model.connect_to_db(server.app)
+model.db.create_all()
 
