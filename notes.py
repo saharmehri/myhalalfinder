608c5c2c91391698273b4ce04b6da0b5b86b2728
Sahar Mehri <smehri@alumni.scu.edu>
2023-10-07 12:42:19 -0700

    Update notes

--- notes.py
+++ notes.py
@@ -164,3 +164,11 @@
 #                         <span class="fa fa-star"></span>
 #                     {% endif %}
 #                   {% endfor %} 
+
+
+
+# How to git push when getting a non-fast forward error:
+#1> git pull --rebase origin <name-of-the-remote-branch>
+    #This will fetch the remote changes in to your local branch . On top of that it will apply your local commits .
+#2> git push origin <name-of-the-remote-branch>
+    #This will apply your local changes in the local copy of the remote branch to the actual remote branch repo in git
